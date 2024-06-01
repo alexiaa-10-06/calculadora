0890087956ec39ec087b6fccb85d388324fbe494
MAURICIO CUECUECHA HERNANDEZ <168512051+Mauhcue28@users.noreply.github.com>
2024-06-01 17:30:20 -0600

    Update calculadora2.cpp

--- calculadora2.cpp
+++ calculadora2.cpp
@@ -3,4 +3,11 @@ using namespace std;
 
 int main() 
 {
+  char op;
+float num1, num2;
+cout<<"ingrese el primer numero";
+  cin>>num;
+cout<<"ingrese el segundo numero";
+cin>>num2;
+cout<<"ingrese el operador
   
